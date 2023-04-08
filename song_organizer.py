dd04b2c0d49bb16fd18b8b860f260414c064cc39
RyanVauk <89154035+RyanVauk@users.noreply.github.com>
2023-04-08 01:29:47 -0500

    Updated song_organier.py (2.0)
    -Added docstrings for all of the functions
    -Updated algorithm to match songs to the downloaded setlist for increased accuracy
         -Matches files with the metadata name instead of the actual name of the file for increased abstraction
         -If one to one matching fails, algorithm will now go through each word in the file metadata name and check if that is in the setlist song name
         -Removed any chance for an infinite loop
    -Added comments before large complicated blocks of code to explain what it is doing
    -Fixed an error where the folder that was being processed was not printed to the console
    -Cleaned up code and got rid of unnecessary or extraneous bits
    -Eliminated old bits of useless code that were stored in comments
    -Tested on more concerts and added edge cases accordingly

--- song_organizer.py
+++ song_organizer.py
@@ -6,34 +6,114 @@ import os, stat
 import shutil
 
 def google_search(search_term):
+    """Takes in a search term and uses the googlesearch library to search for it. It will then return the first url of the resulting search.
+
+    ### Parameters
+    1. search_term : str
+        - The term to be searched for
+
+    ### Returns
+    - url : str
+        - The first url of the search
+    """
     for j in search(search_term, tld="co.in", num = 1):
         return j
 
 def convert_to_american(date):
+    """Takes in a date in format year/month/day and returns it as the American date format (month/day/year)
+
+    ### Parameters
+    1. date : str
+        - Date in format (year/month/day)
+
+    ### Returns
+    - date : str
+        - Date in American format (month/day/year)
+    """
     year = date[2:4]
     month = date[5:7]
     day = date[8:10]
     return month + "/" + day + "/" + year
 
 def change_album(dir, new_album):
+    """Changes album of the given directory song file to new_album
+
+    ### Parameters
+    1. dir : str
+        - Directory of the song file to change
+    2. new_album : str
+        - The new album to replace the old one
+    """
     file=MP4(dir)
     file[r'©alb'] = new_album
     file.save()
 
 def change_name(dir, new_name):
+    """Changes thename of the given directory song file to new_name
+
+    ### Parameters
+    1. dir : str
+        - Directory of the song file to change
+    2. new_name : str
+        - The new name to replace the old one
+    """
     file=MP4(dir)
     file[r'©nam'] = new_name
     file.save()
 
+def get_name(dir):
+    """Gets the name of the given directory song file 
+
+    ### Parameters
+    1. dir : str
+        - Directory of the song file to return the name of
+        
+    ### Returns
+    - name : str
+        - Metadata name of the given directory file
+    """
+    file=MP4(dir)
+    return file[r'©nam'][0]
+
 def get_album(dir):
+    """Gets album of the given directory song file 
+
+    ### Parameters
+    1. dir : str
+        - Directory of the song file to return the album of
+        
+    ### Returns
+    - album : str
+        - Metadata album of the given directory file
+    """
     file=MP4(dir)
     return file[r'©alb'][0]
 
 def get_artist(dir):
+    """Gets artist of the given directory song file 
+
+    ### Parameters
+    1. dir : str
+        - Directory of the song file to return the artist of
+        
+    ### Returns
+    - artist : str
+        - Metadata artist of the given directory file
+    """
     file=MP4(dir)
     return file[r"©ART"][0]
 
 def get_readable_text_from_webpage(url):
+    """Returns the text from the given url webpage
+
+    ### Parameters
+    1. url : str
+        - url to scrape text from
+        
+    ### Returns
+    - text : str
+        - Text from the url webpage
+    """
     html = urlopen(url).read()
     soup = BeautifulSoup(html, features="html.parser")
     for script in soup(["script", "style"]):
@@ -49,6 +129,16 @@ def get_readable_text_from_webpage(url):
     return text
 
 def transform_webpage_to_setlist(text):
+    """Transforms basic webpage text to a readable setlist
+
+    ### Parameters
+    1. text : str
+        - Webpage text
+        
+    ### Returns
+    - list : return_array
+        - Array of songs in the setlist
+    """
     return_array = []
     encore = 0
     text = text[text.index("Set 1"):text.index("I was there")].replace("\n(>)", " >").replace("\n(>", " > \n")
@@ -95,6 +185,20 @@ def transform_webpage_to_setlist(text):
     return return_array
 
 def progress_bar(current, total, bar_length=20):
+    """Returns a progress bar
+
+    ### Parameters
+    1. current : int
+        - Current percentange of the progress bar that is filled
+    2. total : int
+        - Max percetange of the progress bar that can be filled
+    3. bar_length : int
+        - How long the bar should be on the console
+        
+    ### Returns
+    - str : progressbar
+        - A progress bar with the given parameters taken into account
+    """
     fraction = current / total
 
     arrow = int(fraction * bar_length - 1) * '-' + '>'
@@ -105,12 +209,25 @@ def progress_bar(current, total, bar_length=20):
     return f'Progress: [{arrow}{padding}] {int(fraction*100)}%'
 
 def album_to_searchterm(artist, album):
+    """Changes thename of the given directory song file to new_name
+
+    ### Parameters
+    1. artist : str
+        - Artist to search for
+    2. album : str
+        - Name of the album downloaded
+
+    ### Returns
+    - searchterm : str
+        - Searchterm to find the setlist.fm webpage of the given artist, album, date and location
+    """
     date = convert_to_american(album[:album.index(" ")])
     location = album[album.index(" ") + 1:]
     return "Setlistfm " + artist + " " + date + " " + location
 
 # ----------------------------------------------------------------------------------------------------------
 try:
+    #Create a directory for the user to put all concert folders in to be processed
     starting_directory = "C:\\Users\\ryanv\\Desktop\\_organizesongs"
     os.mkdir(starting_directory)
     os.chdir(starting_directory)
@@ -121,39 +238,67 @@ except FileExistsError:
 except:
     print("Unsuccessfully created folder")
 
+#Count number of concert folders in _organizesongs
 total_folders = len(os.listdir())
+
 folder_iterations = 1
-for concert_folder in os.listdir(): #folders
-    #print("\nChanging " + concert_folder + " (" + str(folder_iterations) + "/" + str(total_folders) + ")")
-    #print(progress_bar(0, 100) + "                                                       ", end='\r', flush=True)
+
+#For concert folder in _organizesongs
+for concert_folder in os.listdir():
+    print("\nChanging " + concert_folder + " (" + str(folder_iterations) + "/" + str(total_folders) + ")")
+    print(progress_bar(0, 100) + "                                                       ", end='\r', flush=True)
     os.chdir(starting_directory + "\\" + concert_folder)
+
+    #Get the album for the current concert folder
     album = get_album(starting_directory + "\\" + concert_folder + "\\" +os.listdir()[0])
+
+    #Get the date for the current concert folder
     date = convert_to_american(album[:album.index(" ")])
+
+    #Get the artist for the current concert folder
     artist = get_artist(starting_directory + "\\" + concert_folder + "\\" +os.listdir()[0])
-
     print(progress_bar(5, 100) + "                                                       ", end='\r', flush=True)
-    searchterm = album_to_searchterm(artist, album)
 
+    #Create a searchterm with the album to be googled
+    searchterm = album_to_searchterm(artist, album)
     print(progress_bar(6, 100) + "                                                       ", end='\r', flush=True)
+
+    #Find the url of the setlist on setlist.fm
     url_setlist = google_search(searchterm)
-    os.remove(starting_directory + "\\" + concert_folder + "\\.google-cookie") # Mysterious file
+
+    #Remove a mysterious file
+    os.remove(starting_directory + "\\" + concert_folder + "\\.google-cookie")
+
+    #Get readable text from the downloaded setlist information
     text = get_readable_text_from_webpage(url_setlist)
+
+    #Get venue for later use
     venue = text[text.index("at")+3:text.index(", ")] + album[-4:]
+
+    #Transfom the readable text to a functioning setlist
     setlist = transform_webpage_to_setlist(text)
     count = 0
     song_list = os.listdir()
     missing_songs = ""
     duplicate_songs = ""
     print(progress_bar(25, 100) + "                                                       ", end='\r', flush=True)
-    for song in setlist: #songs in setlist
-        print(progress_bar(((((count+1)/len(song_list))*70) + 25), 100) + "                                                       ", end='\r', flush=True)
-        if(").m4a" in song_list[count] or " - Copy.m4a" in song_list[count]):
-            duplicate_songs += song_list[count] + ", "
-            count += 1
 
+    #For song string in the downloaded setlist
+    for song in setlist:
+        print(progress_bar(((((count+1)/len(song_list))*70) + 25), 100) + "                                                       ", end='\r', flush=True)
+        
+        #If the song is obviously a duplicate, skip it and add it to the duplicates list to be shown at the end of processing
+        try:
+            if(").m4a" in song_list[count] or " - Copy.m4a" in song_list[count]):
+                duplicate_songs += song_list[count] + ", "
+                count += 1
+        except:
+            break
+        iterations = 0
         while True:
-            #print(song_list[count][song_list[count].index("_") + 4:song_list[count].index(".m")].upper().replace("THE_", "").replace("_LAMA_", "LAMMA").replace("_", "") + ":" + song.replace("'", "").replace(".", "").replace("Pt 1", "Pt I").replace("Pt 2", "Pt II").replace("ñ", "").replace(" (", " ").replace(")", "").upper().replace("THE ", "").replace(" ", ""))
-            if(song_list[count][song_list[count].index("_") + 4:song_list[count].index(".m")].upper().replace("THE_", "").replace("_LAMA_", "LAMMA").replace("_", "")  in song.replace("'", "").replace(".", "").replace("-", "").replace("Pt 1", "Pt I").replace("Pt 2", "Pt II").replace("ñ", "").replace(" (", " ").replace(")", "").upper().replace("THE ", "").replace(" ", "")):
+
+            #Check if the directory song name is in the downloaded setlist
+            if((get_name(song_list[count]).upper() in song.upper()) or (song[:song.rindex(":")].replace(" (Reprise)", "").upper() in get_name(song_list[count]).upper())):
                 if(">" in song):
                     change_name(starting_directory + "\\" + concert_folder + "\\" + os.listdir()[count], song[:song.index(" >")+2])
                 else:
@@ -164,30 +309,66 @@ for concert_folder in os.listdir(): #folders
                 break
 
             else:
+                #Check each word in the directory song name, if they are all there, then there is a match between the directory song and the setlist song!
+                possible_fix = True
+                for i in get_name(song_list[count]).replace(".", " ").split(" "):
+
+                    #Edge cases
+                    if(i == "I"):
+                        continue
+                    if(i == "II"):
+                        continue
+                    if(i == "Lama"):
+                        continue
+                    
+
+                    if(i.upper() not in song.upper()):
+                        print(i + " is not in " + song)
+                        possible_fix = False
+                #If the above for loop gives a true value, fix the album and name of the directory song
+                if(possible_fix):
+                    if(">" in song):
+                        change_name(starting_directory + "\\" + concert_folder + "\\" + os.listdir()[count], song[:song.index(" >")+2])
+                    else:
+                        change_name(starting_directory + "\\" + concert_folder + "\\" + os.listdir()[count], song[:song.index(":")])
+                    new_album = date + " | " + song[song.index("λ") + 1:] + " | " + venue
+                    change_album(starting_directory + "\\" + concert_folder + "\\" + os.listdir()[count], new_album)
+                    count += 1
+                    break
+                
+                #If none of the above works, try skipping a song in the directory (to skip past any duplicates that could mess with the matching)
                 try:
-                    if(song_list[count+1][song_list[count+1].index("_") + 4:song_list[count+1].index(".m")].replace("_", " ").upper().replace("THE ", "").replace(" ", "") in song.replace("'", "").replace(".", "").replace("Pt 1", "Pt I").replace("Pt 2", "Pt II").replace("ñ", "").replace(" (", " ").replace(")", "").upper().replace("THE ", "").replace(" ", "")):
+                    if((get_name(song_list[count+1]).upper() in song.upper()) or (song[:song.rindex(":")].replace(" (Reprise)", "").upper() in get_name(song_list[count+1]).upper())):
                         change_album(starting_directory + "\\" + concert_folder + "\\" + os.listdir()[count], new_album)
                         count += 1
-                    else:
+                    elif(iterations == 1):
                         missing_songs += song + ", "
                 except:
                     pass
+            #Prevent infinite loops
+            iterations += 1
+            if(iterations >= 5):
+                break
 
 
+    #If there is any missing songs or duplicate songs, print it to the console and make a log file containing that information
     if(len(missing_songs) + len(duplicate_songs) > 0):
+        print("missing songs: " + missing_songs + "\n" + "duplicate songs: " + duplicate_songs + "\n")
         print(progress_bar(((((count+1)/len(song_list))*70) + 25), 100) + "                                                       ", end='\r', flush=True)
         with open('log.txt', 'w', encoding="utf-8") as f:
             f.write("missing songs: " + missing_songs + "\n" + "duplicate songs: " + duplicate_songs)
     os.chdir(starting_directory)
     print(progress_bar(98, 100) + "                                                       ", end='\r', flush=True)
+    
+    #Move completed concert file out of _organizesongs
     shutil.move(starting_directory + "\\" + concert_folder, starting_directory[:starting_directory.rindex("\\")])
     folder_iterations += 1
     print(progress_bar(100, 100) + "                                                       ")
-    if(len(missing_songs) + len(duplicate_songs) > 0):
-        pass
-        print("missing songs: " + missing_songs + "\n" + "duplicate songs: " + duplicate_songs + "\n")
+
 os.chdir(starting_directory[:starting_directory.index("\\")+1])
 os.chmod(starting_directory, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+#Remove _organizesongs folder
 shutil.rmtree(starting_directory, ignore_errors=False)
 print("Successfully deleted " + starting_directory + "!")
 print("------------------------------\nProgram Complete!")
